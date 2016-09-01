f0758efb1c15036ee344846be53e4a4682096b88
Vishaan <vtiwarie@yahoo.com>
2016-09-01 19:33:55 -0400

    create mainactivity

--- app/src/main/java/com/apppartner/androidprogrammertest/MainActivity.java
+++ app/src/main/java/com/apppartner/androidprogrammertest/MainActivity.java
@@ -1,17 +1,25 @@
 package com.apppartner.androidprogrammertest;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity
 {
+    private TextView mHeader;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHeader = (TextView) findViewById(R.id.textView);
+        //set font
+        mHeader.setTypeface(Typeface.createFromAsset(getAssets(), "Jelloween - Machinato Bold.ttf"));
     }
 
     public void onLoginButtonClicked(View v)
